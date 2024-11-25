2ef0030d7357665c332d319881d21ac278583eb6
gl0git <williamcolinsgeorge@gmail.com>
2024-11-25 02:51:27 -0500

    social auth w/ clerk

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,16 +1,24 @@
 import { Header } from "@/app/components/Header"
 import { Sidebar } from "@/app/components/Sidebar"
+import { SignedIn, SignedOut, SignIn } from "@clerk/nextjs"
 
 export default function DashboardLayout({ children }) {
   return (
     <div className="flex h-screen">
-      <Sidebar />
-      <div className="flex-1 flex flex-col overflow-hidden">
-        <Header />
-        <main className="flex-1 overflow-y-auto">
-          {children}
-        </main>
-      </div>
+      <SignedIn>
+        <Sidebar />
+        <div className="flex-1 flex flex-col overflow-hidden">
+          <Header />
+          <main className="flex-1 overflow-y-auto">
+            {children}
+          </main>
+        </div>
+      </SignedIn>
+      <SignedOut>
+        <div className="flex items-center justify-center w-full">
+          <SignIn routing="hash" />
+        </div>
+      </SignedOut>
     </div>
   )
 }
--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -19,13 +20,15 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning>
+        <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         suppressHydrationWarning
       >
         {children}
       </body>
     </html>
+    </ClerkProvider>
   );
 }
